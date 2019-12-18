9ca3d54c9639d83078b31d3706089104487c840c
udubnate <natebruneau@gmail.com>
2019-12-18 11:42:57 -0800

    completed Day 1

---
+++ 30DaysofCoding/Day1_DataTypes.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+inputi = int(input())
+inputd = float(input())
+inputs = str(input())
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+sumi = i + inputi
+print(sumi)
+# Print the sum of the double variables on a new line.
+sumd = inputd + d
+print(sumd)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+stringi = s + inputs
+print(stringi)
