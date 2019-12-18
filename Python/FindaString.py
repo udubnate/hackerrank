e22946be11e2ad77d7ea0dbb02659ab4182ade72
udubnate <natebruneau@gmail.com>
2019-12-18 13:05:18 -0800

    had to take two attempts at this one, but got it on the second try

---
+++ Python/FindaString.py
@@ -0,0 +1,22 @@
+# May try to use this code
+#Now, trying to find the index of a substring between specified indexes only
+#print "Now, trying to find a substring between specified indexes only: looking for 'l' between 4 and 9"
+# print testString1.find('l',4,9)
+
+def count_substring(string, sub_string):
+
+    sublen = len(sub_string)
+    count = 0
+    for i in range(0, len(string)):
+        if sub_string == string[i:(sublen+i)]:
+            count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
