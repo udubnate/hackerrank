4bf32ecf9661e04253a471c3b9f0c7a8e942b71d
udubnate <natebruneau@gmail.com>
2019-12-17 20:48:22 -0800

    optimized/refactored

--- Python/Lists.py
+++ Python/Lists.py
@@ -3,24 +3,16 @@ if __name__ == '__main__':
 
 arr = []
 
-
-def remove(e):
-    for item in arr:
-        if int(item) == int(e):
-            arr.remove(item)
-            break
-
 for _ in range(N):
     command = input()
     cmd = command.split(' ')
 
-    #insert
     if cmd[0] == "insert":
         arr.insert(int(cmd[1]),int(cmd[2]))
     elif cmd[0] == "print":
         print(arr)
     elif cmd[0] == "remove":
-        remove(cmd[1])
+       arr.remove(int(cmd[1]))
     elif cmd[0] == "append":
         arr.append(int(cmd[1]))
     elif cmd[0] == "sort":
@@ -30,4 +22,3 @@ for _ in range(N):
     elif cmd[0] == "reverse":
         arr.reverse()
 
-#    print(cmd[0])
