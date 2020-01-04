346aadc06ec6e8fdb85eb94db81b7280f3f0ffbe
Nate Bruneau <natebruneau@gmail.com>
2020-01-04 11:13:37 -0800

    Day 7 - a quick one

---
+++ 30DaysofCoding/Day7_Arrays.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+
+for x in range(len(arr)):
+    print(arr[len(arr)-x-1], end=" ")
